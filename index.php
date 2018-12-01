9a4a6d4de4b86b023575e3ddf29dd8ff4d224338
Prithwiraj Dutta <duttaprithwiraj9@gmail.com>
2018-12-01 23:55:51 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  </head>
+  <body>
+
+  </body>
+  <script
+  src="https://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  crossorigin="anonymous"></script>
+  <script>
+    let a = [];
+    $(document).ready(function(){
+        fetch(`http://localhost/phpchartjs/api.php`)
+                .then(res=>res.json())
+                .then(data => {
+                    data.records.map(entry => {
+                    return a.push(entry);
+                    })
+                })
+                .catch(err => {console.log(err)});
+                console.log(a);
+    });
+  </script>
+</html>
